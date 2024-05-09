cdd499f50c7980bf19c0e4e5ccd0f68dc715f786
LXSCA <lucas.edr27@gmail.com>
2024-05-09 20:07:47 -0300

    feat: desafio solucionado + menu realizado feito

--- Program.cs
+++ Program.cs
@@ -6,20 +6,79 @@ Console.OutputEncoding = Encoding.UTF8;
 // Cria os modelos de hóspedes e cadastra na lista de hóspedes
 List<Pessoa> hospedes = new List<Pessoa>();
 
-Pessoa p1 = new Pessoa(nome: "Hóspede 1");
-Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+Console.WriteLine("+------------------------------------------------------------------------------+");
+Console.WriteLine("|                                  H O T E L                                   |");
+Console.WriteLine("+-------------------------------------------------------------------------------");
+Console.WriteLine("|                                   SUÍTES                                     |");
+Console.WriteLine("+------------------------------------------------------------------------------+");
+Console.WriteLine("| 1 - Suíte Padrão     | Capacidade: 1 hóspede    | Valor da diária: R$ 100,00 |");
+Console.WriteLine("|                                                                              |");
+Console.WriteLine("| 2 - Suíte Executiva  | Capacidade: 2 hóspedes   | Valor da diária: R$ 150,00 |");
+Console.WriteLine("|                                                                              |");
+Console.WriteLine("| 3 - Suíte Premium    | Capacidade: 2 hóspedes   | Valor da diária: R$ 250,00 |");
+Console.WriteLine("|                                                                              |");
+Console.WriteLine("| 4 - Suíte Master     | Capacidade: 2 hóspedes   | Valor da diária: R$ 300,00 |");
+Console.WriteLine("|                                                                              |");
+Console.WriteLine("| 5 - Suíte Familiar   | Capacidade: 4 hóspedes   | Valor da diária: R$ 500,00 |");
+Console.WriteLine("|                                                                              |");
+Console.WriteLine("| 6 - Master Superior  | Capacidade: 4 hóspedes   | Valor da diária: R$ 650,00 |");
+Console.WriteLine("+------------------------------------------------------------------------------+\n");
+Console.Write("Escolha a suíte: ");
+string escolha = Console.ReadLine();
 
-hospedes.Add(p1);
-hospedes.Add(p2);
+Suite suite;
 
-// Cria a suíte
-Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
+switch (escolha)
+{
+    case "1":
+        suite = new Suite(tipoSuite: "Padrão", capacidade: 1, valorDiaria: 100.00M);
+        break;
+    case "2":
+        suite = new Suite(tipoSuite: "Executiva", capacidade: 2, valorDiaria: 150.00M);
+        break;
+    case "3":
+        suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 250.00M);
+        break;
+    case "4":
+        suite = new Suite(tipoSuite: "Master", capacidade: 2, valorDiaria: 300.00M);
+        break;
+    case "5":
+        suite = new Suite(tipoSuite: "Familiar", capacidade: 4, valorDiaria: 500.00M);
+        break;
+    case "6":
+        suite = new Suite(tipoSuite: "Superior", capacidade: 4, valorDiaria: 650.00M);
+        break;
+    default:
+        throw new Exception("Erro. Suíte inexistente.");
+        break;
+}
+
+Console.Write("Insira a quantidade de Hóspedes: ");
+int quantidadeDeHospedes = Int32.Parse(Console.ReadLine());
+Console.Clear();
+
+for (int i = 0; i < quantidadeDeHospedes; i++)
+{
+    Console.Write("Insira nome do hóspede: ");
+    string nomeHospede = Console.ReadLine();
+    Console.Write("Insira Sobrenome do hóspede: ");
+    string sobrenomeHospede = Console.ReadLine();
+    hospedes.Add(new Pessoa(nomeHospede, sobrenomeHospede));
+    Console.Clear();
+}
+
+Console.Write("Insira a quantidade de diárias: ");
+int quantidadeDeDias = Int32.Parse(Console.ReadLine());
 
 // Cria uma nova reserva, passando a suíte e os hóspedes
-Reserva reserva = new Reserva(diasReservados: 5);
+Reserva reserva = new Reserva(diasReservados: quantidadeDeDias);
 reserva.CadastrarSuite(suite);
 reserva.CadastrarHospedes(hospedes);
 
 // Exibe a quantidade de hóspedes e o valor da diária
 Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
-Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+foreach (Pessoa i in hospedes)
+{
+    Console.WriteLine(" - " + i.NomeCompleto);
+}
+Console.WriteLine($"\nValor diária: {reserva.CalcularValorDiaria()}");
--- README.md
+++ README.md
@@ -1,21 +1,24 @@
-# DIO - Trilha .NET - Explorando a linguagem C#
-www.dio.me
+# Projeto - Sistema de Hospedagem
+## Feito durante o Bootcamp de .NET developer da DIO
 
-## Desafio de projeto
-Para este desafio, você precisará usar seus conhecimentos adquiridos no módulo de explorando a linguagem C#, da trilha .NET da DIO.
+[Desafio original](https://github.com/digitalinnovationone/trilha-net-explorando-desafio).
 
-## Contexto
-Você foi contratado para construir um sistema de hospedagem, que será usado para realizar uma reserva em um hotel. Você precisará usar a classe Pessoa, que representa o hóspede, a classe Suíte, e a classe Reserva, que fará um relacionamento entre ambos.
+[README original](Desafio.md).
 
-O seu programa deverá cálcular corretamente os valores dos métodos da classe Reserva, que precisará trazer a quantidade de hóspedes e o valor da diária, concedendo um desconto de 10% para caso a reserva seja para um período maior que 10 dias.
+## Sobre o projeto:
 
-## Regras e validações
+O desafio consiste na criação de um programa de hospedagem para um Hotel, utilizando a classe `Pessoa`, para representar o hóspede, `Suíte`, para o quarto, e a classe `Reserva`, que fará um relacionamento entre as duas classes.
+
+O programa precisa calcular corretamente as diárias, e conceder um desconto de 10% no valor quando a quantidade de diárias fosse maior ou igual a 10.
+
+## Regras:
+
 1. Não deve ser possível realizar uma reserva de uma suíte com capacidade menor do que a quantidade de hóspedes. Exemplo: Se é uma suíte capaz de hospedar 2 pessoas, então ao passar 3 hóspedes deverá retornar uma exception.
-2. O método ObterQuantidadeHospedes da classe Reserva deverá retornar a quantidade total de hóspedes, enquanto que o método CalcularValorDiaria deverá retornar o valor da diária (Dias reservados x valor da diária).
+
+2. O método `ObterQuantidadeHospedes` da classe `Reserva` deverá retornar a quantidade total de hóspedes, enquanto que o método `CalcularValorDiaria` deverá retornar o valor da diária (Dias reservados x valor da diária).
+
 3. Caso seja feita uma reserva igual ou maior que 10 dias, deverá ser concedido um desconto de 10% no valor da diária.
 
-
-![Diagrama de classe estacionamento](diagrama_classe_hotel.png)
-
 ## Solução
-O código está pela metade, e você deverá dar continuidade obedecendo as regras descritas acima, para que no final, tenhamos um programa funcional. Procure pela palavra comentada "TODO" no código, em seguida, implemente conforme as regras acima.
+
+O código originalmente estava feito pela metade, então fiz as devidas correções, e além disso, fiz a criação de um menu, onde o usuário pode escolher a suíte que deseja e inserir a quantidade de hóspedes que serão adicionados. Caso seja maior que a quantidade limite do quarto, a Exception é retornada da mesma forma. 
